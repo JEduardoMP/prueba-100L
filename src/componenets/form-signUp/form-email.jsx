2113bc5e198acef14098862924ee5933b7e4fd67
Jose Eduardo <eduardomarque2009@gmail.com>
2021-12-19 14:33:04 -0600

    Realizado servicio para signup, pero con problemas para autenticar, muestra acceso negado a localhost:3000

--- src/componenets/form-signUp/form-email.jsx
+++ src/componenets/form-signUp/form-email.jsx
@@ -20,75 +20,84 @@ const FormEmail = ({ handleDataInfo, handleDataSmall }) => {
     }
   }, [inputInfo]);
 
+  const handleForm = async (e) => {
+    e.preventDefault();
+    const newRegister = {
+      email: inputInfo.email,
+      password: inputInfo.password,
+    };
+    await handleSingnUp(newRegister);
+  };
+
   const { width } = WindowSizeInfo();
 
   return (
-    <div className="my-8">
-      <form action="" onSubmit={handleSingnUp()}>
-        <h2 className="font-bold text-lg">Crea tu cuenta</h2>
-        <br />
-        <p className="text-base">
-          Al aceptar crear una cuenta en 100 Ladrillos aceptas nuestro{" "}
-          <Link className="text-blue-400" to="/" exact>
-            Aviso de Privacidad
-          </Link>
-          ,{" "}
-          <Link className="text-blue-400" to="/" exact>
-            Derechos Arco
-          </Link>{" "}
-          y nuestros{" "}
-          <Link className="text-blue-400" to="/" exact>
-            Términos y Condiciones.
-          </Link>
-        </p>
-        <br />
-        <div className="flex flex-col">
-          <label htmlFor="email" className="mb-2">
-            ¿Cuál es tu correo electrónico?
-          </label>
+    <form onSubmit={handleForm}>
+      <h2 className="font-bold text-lg">Crea tu cuenta</h2>
+      <br />
+      <p className="text-base">
+        Al aceptar crear una cuenta en 100 Ladrillos aceptas nuestro{" "}
+        <Link className="text-blue-400" to="/" exact>
+          Aviso de Privacidad
+        </Link>
+        ,{" "}
+        <Link className="text-blue-400" to="/" exact>
+          Derechos Arco
+        </Link>{" "}
+        y nuestros{" "}
+        <Link className="text-blue-400" to="/" exact>
+          Términos y Condiciones.
+        </Link>
+      </p>
+      <br />
+      <div className="flex flex-col">
+        <label htmlFor="email" className="mb-2">
+          ¿Cuál es tu correo electrónico?
+        </label>
+        <input
+          className="rounded border border-gray-400 py-2 pl-1.5 "
+          type="email"
+          name="email"
+          id="email"
+          onChange={(e) =>
+            setInputInfo({ ...inputInfo, email: e.target.value })
+          }
+          placeholder="tu@correo.com"
+          required
+        />
+      </div>
+      <br />
+      <div className="flex flex-col">
+        <label htmlFor="password" className="mb-2">
+          Ingrese una contraseña
+        </label>
+        <div className=" w-full relative">
           <input
-            className="rounded border border-gray-400 py-2 pl-1.5 "
-            type="email"
-            name="email"
-            id="email"
+            className="rounded border border-gray-400 py-2 pl-1.5 w-full "
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Contraseña"
             onChange={(e) =>
-              setInputInfo({ ...inputInfo, email: e.target.value })
+              setInputInfo({ ...inputInfo, password: e.target.value })
             }
-            placeholder="tu@correo.com"
             required
           />
         </div>
-        <br />
-        <div className="flex flex-col">
-          <label htmlFor="password" className="mb-2">
-            Ingrese una contraseña
-          </label>
-          <div className=" w-full relative">
-            <input
-              className="rounded border border-gray-400 py-2 pl-1.5 w-full "
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Contraseña"
-              onChange={(e) =>
-                setInputInfo({ ...inputInfo, password: e.target.value })
-              }
-              required
-            />
-          </div>
-        </div>
-      </form>
+      </div>
       {inputInfo.email === "" && inputInfo.password === "" ? (
-        <br />
+        <span></span>
       ) : (
-        <PasswordRequirements pass={inputInfo.password} />
+        <div>
+          <PasswordRequirements pass={inputInfo.password} />
+            <input
+              className="mt-4 bg-[#eaeaea] text-[#8c8c8c] px-4 py-2 rounded"
+              type="submit"
+              value="Siguiente"
+            />
+        </div>
       )}
-      <input
-        className="mt-4 bg-[#eaeaea] text-[#8c8c8c] px-4 py-2 rounded"
-        type="submit"
-        value="Siguiente"
-      />
-    </div>
+    </form>
   );
 };
 
--- src/services/signUp.js
+++ src/services/signUp.js
@@ -1,3 +1,20 @@
-export const handleSingnUp = () => {
+import axios from "axios";
 
-}
+export const handleSingnUp = async (info) => {
+  try {
+    console.log(info);
+    const response = await axios({
+      url: "https://frontend-recruiting.100ladrillos.com/api/signUp",
+      mode: "no-cors",
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify(info),
+    });
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(`El error es: ${error}`);
+  }
+};
